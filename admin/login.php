860ab4024a6ecb437b7d89f3ebd569d92cf03d4b
Adriano baram <alfa_bar@hotmail.com>
2021-11-01 15:35:16 -0300

    Atualizado

---
+++ admin/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login - Comida sistema Pedido</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <div class="login">
+            <h1 class="text-center">Login</h1>
+            <!-- Formulario de Login aqui -->
+            <form class="text-center" action="" method="POST">
+                <label for="username">Usuário:</label>
+                <input type="text" name="username" placeholder="Entre com Usuário"><br><br>
+                <label for="password">Senha:</label>
+                <input type="password" name="password" placeholder="Senha"><br><br>
+
+                <input type="submit" name="submit" value="login" class="btn-primary">
+            </form>
+            <!-- Formulario de Login aqui Fim-->
+
+            <p class="text-center">Criado por - <a href="">Adriano Baram</a></p>
+        </div>
+        <script src="" async defer></script>
+    </body>
+</html>
