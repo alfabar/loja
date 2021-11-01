2c98338f23bc2e8b4cb782dec0db2359519fe5c5
Adriano baram <alfa_bar@hotmail.com>
2021-11-01 01:18:57 -0300

    ok

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,25 +5,27 @@
 
   <div class="wrapper">
     <h1>Manage admin</h1>
-    <br/>
-    <?php 
-    if(isset($_SESSION['add']))
-    {
+    <br />
+    <?php
+    if (isset($_SESSION['add'])) {
       echo $_SESSION['add']; // Mostrando a mensagen de sessão
       unset($_SESSION['add']); // Removendo mensagem de sessão
     }
 
-    if(isset($_SESSION['delete']))
-
-      {
-        echo $_SESSION['delete'];
-        unset($_SESSION['delete']);
-      }
-
-    ;?>
+    if (isset($_SESSION['delete'])) {
+      echo $_SESSION['delete'];
+      unset($_SESSION['delete']);
+    } 
+    
+    if (isset($_SESSION['update']))
+    {
+      echo $_SESSION['update'];
+      unset($_SESSION['update']);
+    }
+    ?>
     <!-- Botão do administrador -->
     <a class="btn-primary" href="add-admin.php">Adicionar Adm</a>
-    <br/><br/>
+    <br /><br />
 
     <table class="tbl-full">
       <tr>
@@ -32,59 +34,51 @@
         <th>Usuario</th>
         <th>Ações</th>
       </tr>
-      <?php 
+      <?php
 
-        // Consulta para obter todos os administradores
+      // Consulta para obter todos os administradores
       $sql = "SELECT * FROM tbl_admin";
-        // executando a query
+      // executando a query
       $res = mysqli_query($conn, $sql);
 
-        // Checando a query para ver se foi consultada 
-      if($res==TRUE)
-      {
+      // Checando a query para ver se foi consultada 
+      if ($res == TRUE) {
         // CONTAGEM PARA VERIFICAR SE TEMOS DADOS NO BANCO DE DADOS
         $count = mysqli_num_rows($res);
-        $sn=1; //criando a variavel
+        $sn = 1; //criando a variavel
 
 
         // verifique o número de linhas
-        if($count>0)
-        {
-       // temos dados no banco de dados
-         while($rows=mysqli_fetch_assoc($res))
-          {
+        if ($count > 0) {
+          // temos dados no banco de dados
+          while ($rows = mysqli_fetch_assoc($res)) {
             // usando o loop while para obter os dados do banco de dados
             // e o loop while será executado enquanto tivermos dados no banco de dados
             // obter dados individuais
-            $id=$rows['id'];
-            $full_name=$rows['full_name'];
-            $username=$rows['username'];
+            $id = $rows['id'];
+            $full_name = $rows['full_name'];
+            $username = $rows['username'];
 
             // exibe os valores na tabela
-            ?>
+      ?>
 
-      <tr>
-        <td><?php echo $sn++;?></td>
-        <td><?php echo $full_name;?></td>
-        <td><?php echo $username;?></td>
-        <td> <a class="btn-secundary" href="">Atualizar</a>
-          <a class="btn-danger" href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>">delete admin</a></td>
-      </tr>
-            <?php
+            <tr>
+              <td><?php echo $sn++; ?></td>
+              <td><?php echo $full_name; ?></td>
+              <td><?php echo $username; ?></td>
+              <td> <a class="btn-secundary" href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>">Atualizar</a>
+                <a class="btn-danger" href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>">delete admin</a>
+              </td>
+            </tr>
+      <?php
           }
-
-
-
-
-        }
-        else
-        {
-         // não temos dados no banco de dados
+        } else {
+          // não temos dados no banco de dados
         }
       }
-      
+
       ?>
-     
+
     </table>
 
   </div>
---
+++ admin/update-admin.php
@@ -0,0 +1,99 @@
+<?php include('partials/menu.php'); ?>
+
+
+<div class="main-content">
+
+    <div class="wrapper">
+        <h1>Atualize dados admin</h1>
+        <br><br>
+        <?php
+        //1º Pegar o id do admin selecionado
+        $id = $_GET['id'];
+
+        //2º Criar uma consulta SQL para obter detalhes
+        $sql = "SELECT * FROM tbl_admin WHERE id=$id";
+
+        // Executar a query
+        $res = mysqli_query($conn, $sql);
+
+        // Verificar se a consulta foi realizada
+        if ($res == true) {
+            // Verificar se os dados estão disponiveis
+            $count = mysqli_num_rows($res);
+
+            // Verifique se temos dados ou não
+            if ($count == 1) {
+                //Obtenha detalhes
+                //echo "admin disponivel";
+                $row = mysqli_fetch_assoc($res);
+
+                $full_name = $row['full_name'];
+                $username = $row['username'];
+            } else {
+                //redirecione para pagina admn
+                header('location:' . SITEURL . 'admin/manage-admin.php');
+            }
+        }
+        ?>
+
+
+        <form action="" method="post">
+
+            <table class="tbl-30">
+
+                <tr>
+                    <td>Nome Completo:</td>
+                    <td><input type="text" name="full_name" value="<?php echo $full_name; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="username" value="<?php echo $username; ?>"></td>
+                </tr>
+
+                <tr>
+                    <td colspan="2"><input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <input type="submit" name="submit" value="Update Admin" class="btn-secundary">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
+
+<?php
+
+// Checar se o botão submit foi clicado
+if (isset($_POST['submit'])) {
+    // echo " botão clicado";
+    // Obter todos os valores au atualizar
+    $id = $_POST['id'];
+    $full_name = $_POST['full-name'];
+    $username = $_POST['username'];
+
+    //Criar uma consulta sql para atualizar admin
+    $sql = "UPDATE tbl_admin SET
+   full_name = '$full_name',
+   username = '$username'
+   WHERE id='$id'   
+   ";
+
+    // execute a query
+    $res = mysqli_query($conn, $sql);
+
+    //Verificar se a consulta foi relizada com sucesso
+    if ($res == true) {
+        //Admin atualizado com sucesso
+        $_SESSION['update'] = "<div class='success'> Administrador atualizado com sucesso </div>";
+        //Redirecionando para pagina Admin
+        header('location:' . SITEURL . 'admin/manage-admin.php');
+    } else {
+        //Falhou ao atualizar o admin
+        $_SESSION['update'] = "<div class='error'> Administrador Não foi Atualizado </div>";
+        //Redirecionando para pagina Admin
+        header('location:' . SITEURL . 'admin/manage-admin.php');
+    }
+}
+
+?>
+
+<?php include('partials/footer.php'); ?>
