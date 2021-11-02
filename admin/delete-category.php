9a0b7ad4fe4d6b42e8c7b5eece6f4efc8c990193
Adriano baram <alfa_bar@hotmail.com>
2021-11-02 15:49:25 -0300

    Atualização de excluir produto catalogo + imagem server + pasta

---
+++ admin/delete-category.php
@@ -0,0 +1,64 @@
+<?php
+include('../config/constants.php');
+//echo "Delete pagina";
+// Checar se o valor do id e valor nome estão configurados
+if(isset($_GET['id']) AND isset($_GET['image_name']))
+{
+    //obter o valor e deletar
+    echo "Obter valor e deletar ";
+    $id =$_GET['id'];
+    $image_name = $_GET['image_name'];
+
+    // Remover as imagens fisicamente 
+    if($image_name !="")
+    {
+        //imagem disponivel então remova
+        $path = "../images/category/".$image_name;
+        //remover a imagem
+        $remove = unlink($path);
+
+        if($remove==false)
+        {
+            // Definir a mensagem da sessão
+            $_SESSION['remove'] = "<div class='error'>Falhou em remover a imagem da categoria</div>";
+            // redirecionar para categoria
+            header('location:'.SITEURL.'admin/manage-category.php');
+            //Parar o processo
+            die();
+        }
+    }
+
+    // Deletar dados do banco de dados 
+    //Sql Query que deleta do banco de dados
+    $sql = "DELETE FROM tbl_category WHERE id=$id";
+
+    //Executar a query
+    $res = mysqli_query($conn, $sql);
+
+
+    // Verificar se os dados foram deletados
+    if($res==true)
+    {
+        //Definir mensagem e redirecionar
+        $_SESSION['delete'] = "<div class='success'>Categoria deletada com sucesso</div>";
+        //Redirecionar apos exclusão pagina de categorias
+        header('location:'.SITEURL.'admin/manage-category.php');
+    }
+    else
+    {
+        // definir mensagem de erro
+        $_SESSION['delete'] = "<div class='error'>Categoria não foi deletada</div>";
+        //Redirecionar apos exclusão pagina de categorias
+        header('location:'.SITEURL.'admin/manage-category.php');
+
+    }
+
+    // redirecionar para pagina categoria
+}
+else
+{
+
+    //Redirecionar a pagina de Categoria
+    header('location:'.SITEURL.'admin/manage-category.php');
+}
+?>
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -8,7 +8,18 @@
         {
             echo $_SESSION['add'];
             unset($_SESSION['add']);
-        }        
+        }     
+        if(isset($_SESSION['remove']))
+        {
+          echo $_SESSION['remove'];
+          unset($_SESSION['remove']);
+        }  
+
+        if(isset($_SESSION['delete']))
+        {
+          echo $_SESSION['delete'];
+          unset($_SESSION['delete']);
+        }
         ?>
 
       <!-- Botão do administrador -->
@@ -84,7 +95,7 @@
             <td><?php echo $active; ?></td>
             <td> 
               <a class="btn-secundary" href="">Atualizar</a>
-              <a class="btn-danger" href="">Excluir</a>
+              <a class="btn-danger" href="<?php echo SITEURL;?>admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>">Excluir</a>
             </td>
           </tr>
           <?php
