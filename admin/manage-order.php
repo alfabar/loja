051fefe209027038caf5c9d3b9e490bd96e87c46
alfabar <alfa_bar@hotmail.com>
2021-11-28 17:27:07 -0300

    visualizar produtos

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -26,28 +26,63 @@
 
       //Obter os dados do banco
 
-      
+      $sql = "SELECT * FROM tbl_order ORDER BY id DESC";/// Visualizando produtos por id orden decrecente
 
-      
-      ?>
+      // Executar a consulta query
 
-      <tr>
-        <td>1.</td>
-        <td>Adriano Baram</td>
-        <td>alfa_bar</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>        
-        <td> 
-          <a class="btn-secundary" href="">Atualizar Pedido</a>
-          </td>
+      $res = mysqli_query($conn, $sql);
+
+      //contagem das colunas 
+      $count = mysqli_num_rows($res);
+
+      $sn = 1; //criando um numero de serial
+
+      //verificando com uma condição se o numero de colunas da resposta e zero
+      if($count>0)
+      {
+        //Pedido disponivel
+        while($row=mysqli_fetch_assoc($res))
+        {
+        $id = $row['id'];
+        $food = $row['food'];
+        $price = $row['price'];
+        $qtd = $row['qtd'];
+        $total = $row['total'];
+        $order_date = $row['order_date'];
+        $status = $row['status'];
+        $custumer_name = $row['custumer_name'];
+        $custumer_contact = $row['custumer_contact'];
+        $custumer_email = $row['custumer_email'];
+        $custumer_address = $row['custumer_address'];
+      }
+        ?>
+        <tr> 
+          <td><?php echo $sn++; ?></td>
+          <td><?php echo $food; ?></td>
+          <td><?php echo $price; ?></td>
+          <td><?php echo $qtd; ?></td>
+          <td><?php echo $total; ?></td>
+          <td><?php echo $order_date; ?></td>
+          <td><?php echo $status; ?></td>
+          <td><?php echo $custumer_name; ?></td>
+          <td><?php echo $custumer_contact; ?></td>
+          <td><?php echo $custumer_email; ?></td>
+          <td><?php echo $custumer_address; ?></td>        
+          <td> 
+            <a class="btn-secundary" href="">Atualizar Pedido</a>
+            </td>
       </tr>
-      
+
+        <?php
+
+
+      }
+      else
+      {
+        // Pedido não está disponivel 
+        echo "<tr><td class='error' collspan='12'>Pedidos não disponivel</td></tr> ";
+      }      
+      ?>     
     </table>
 </div>
 
